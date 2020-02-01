4980f72c1aed81116aa3c52dac0251be0f505ff6
Ray <ray.10315332@gmail.com>
2020-02-01 09:19:35 +0800

    新增MainMenu

---
+++ Assets/Script/Font.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Font : MonoBehaviour
+{
+
+    public Text Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toMax()
+    {
+        Text.fontSize = 27;
+    }
+
+    public void toMin()
+    {
+
+        Text.fontSize = 12;
+
+    }
+
+    public void toMidum()
+    {
+        Text.fontSize = 18;
+    }
+}
---
+++ Assets/Script/selectInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectInput : MonoBehaviour
+{
+
+    public GameObject p1, p2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
