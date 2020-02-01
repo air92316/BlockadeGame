0f773346e1014d480fba2827b172298d7db2f636
燈塔 <air92316@gmail.com>
2020-02-02 05:06:25 +0800

    [Add] 題目展示音效

---
+++ Assets/Script/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+
+    AudioSource _audioSource;
+
+    private AudioSource AudioSource
+    {
+        get
+        {
+            if (!_audioSource) this._audioSource = this.GetComponent<AudioSource>();
+            return _audioSource;
+        }
+    }
+    public void Play()
+    {
+        AudioSource.Play();
+    }
+}
