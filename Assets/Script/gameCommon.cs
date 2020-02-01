108b7c5c898cdb782ca6e3567042f30fbb9d92b0
燈塔 <air92316@gmail.com>
2020-02-01 17:06:15 +0800

    [Add] gameCommon

---
+++ Assets/Script/gameCommon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameCommon 
+{
+    public static int[] scoreR1 = { 0, 0, 0, 0 };         //第一回合玩家的分數
+    public static int[] scoreR2 = { 0, 0, 0, 0 };         //第一回合玩家的分數
+    public static int[] scoreR3 = { 0, 0, 0, 0 };         //第一回合玩家的分數
+}
